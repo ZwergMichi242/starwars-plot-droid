bf36bad4ed5a4acdd50e500cfa20008199fc07ca
ZwergMichi242 <michaela.anzengruber@antz-development.at>
2019-10-29 00:29:53 +0100

    Updates

---
+++ precache-manifest.ec089a6bb62839191597ef1d4d8abb09.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf54a162e5fe0a1d2cd9b78305b09bf",
+    "url": "/starwars-plot-droid/index.html"
+  },
+  {
+    "revision": "2c247fd11d1502c12b57",
+    "url": "/starwars-plot-droid/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6eb91e9797579f8255e2",
+    "url": "/starwars-plot-droid/static/css/main.515adf26.chunk.css"
+  },
+  {
+    "revision": "2c247fd11d1502c12b57",
+    "url": "/starwars-plot-droid/static/js/2.5fd8c9c8.chunk.js"
+  },
+  {
+    "revision": "6eb91e9797579f8255e2",
+    "url": "/starwars-plot-droid/static/js/main.0ba6eb9f.chunk.js"
+  },
+  {
+    "revision": "abe97cfc7afa6795f539",
+    "url": "/starwars-plot-droid/static/js/runtime-main.98c3a4aa.js"
+  },
+  {
+    "revision": "d3398f0de5c52c1247266b94757d2d71",
+    "url": "/starwars-plot-droid/static/media/Rebellion-oPoA.d3398f0d.ttf"
+  },
+  {
+    "revision": "3f085be9c96d464c8ed2774e89793393",
+    "url": "/starwars-plot-droid/static/media/STARWARS.3f085be9.ttf"
+  },
+  {
+    "revision": "c982ef251eeaa93f501080ca646e5e24",
+    "url": "/starwars-plot-droid/static/media/anakinmono.c982ef25.ttf"
+  },
+  {
+    "revision": "728320ae9b0779633771f3128179aeab",
+    "url": "/starwars-plot-droid/static/media/image5.728320ae.jpg"
+  },
+  {
+    "revision": "48419a412ae68ffb881ddb07b9d7e274",
+    "url": "/starwars-plot-droid/static/media/moonhouse.48419a41.ttf"
+  }
+]);
